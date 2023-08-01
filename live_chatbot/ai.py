35f8422ace8c076bfc89f9d6a8a0b6d945248c6f
Rajaa <rajsingh08471@gmail.com>
2023-08-01 13:16:17 +0530

    new_commit

--- live_chatbot/ai.py
+++ live_chatbot/ai.py
@@ -58,11 +58,69 @@ anthropic = os.getenv("ANTHROPIC_API_KEY")
 memory = ConversationBufferMemory(memory_key="chat_history")
 
 
+
+
+
+from datasets import load_dataset, Audio
+
+# English
+stream_data = load_dataset("mozilla-foundation/common_voice_13_0", "en", split="test", streaming=True)
+stream_data = stream_data.cast_column("audio", Audio(sampling_rate=16000))
+
+# audi
+en_sample = next(iter(stream_data))["audio"]["array"]
+
+# French
+# stream_data = load_dataset("mozilla-foundation/common_voice_13_0", "fr", split="test", streaming=True)
+# stream_data = stream_data.cast_column("audio", Audio(sampling_rate=16000))
+# fr_sample = next(iter(stream_data))["audio"]["array"]
+
+
+from transformers import Wav2Vec2ForCTC, AutoProcessor
+import torch
+
+model_id = "facebook/mms-1b-fl102"
+
+processor = AutoProcessor.from_pretrained(model_id)
+model = Wav2Vec2ForCTC.from_pretrained(model_id)
+
+
+
+
+
+# 'joe keton disapproved of films and buster also had reservations about the media'
+
+
+processor.tokenizer.set_target_lang("fra")
+model.load_adapter("fra")
+
+# inputs = processor(fr_sample, sampling_rate=16_000, return_tensors="pt")
+
+with torch.no_grad():
+    outputs = model(**inputs).logits
+
+ids = torch.argmax(outputs, dim=-1)[0]
+transcription = processor.decode(ids)
+# "ce dernier est volé tout au long de l'histoire romaine"
+processor.tokenizer.vocab.keys()
+print(transcription)
+
+
+
 def stt_function(audio_path):
     audio_file= open(audio_path, "rb")
+    # inputs = processor(audio_file, sampling_rate=16_000, return_tensors="pt")
+    # with torch.no_grad():
+    #     outputs = model(**inputs).logits
+
+    # ids = torch.argmax(outputs, dim=-1)[0]
+    # transcription = processor.decode(ids)
+    # return transcription
     transcript = openai.Audio.transcribe("whisper-1", audio_file)
     print(transcript['text'])
     return transcript
+
+
 
 def llm_reply(query):
     print("Inside LLM")
@@ -70,10 +128,6 @@ def llm_reply(query):
     response = llm_chain.run(query)
     print(response)
     return response
-
-
-
-
 
 
 
